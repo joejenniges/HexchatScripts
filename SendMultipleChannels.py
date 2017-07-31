e7bc76827917669a556c6f47817052d29bc143a0
GunfighterJ <joseph.jenniges@gmail.com>
2017-07-31 11:26:22 -0500

    Allow sending to different servers, or your currently focused server.

--- SendMultipleChannels.py
+++ SendMultipleChannels.py
@@ -2,7 +2,7 @@ import hexchat
 
 __module_name__ = "Send Multiple Channels"
 __module_version__ = "1.0"
-__module_description__ = "Use /msgchannels <channel1,channel2,etc> <msg> to send a message to all channels in the comma separated channel list."
+__module_description__ = "Use /msgchannels <[server:]channel1,channel2,etc> <msg> to send a message to all channels in the comma separated channel list."
 
 hooks = list()
 
@@ -11,7 +11,12 @@ def msgchannels(word, word_eol, userdata):
     channels = channels.split(",")
 
     for channel in channels:
-        hexchat.command("msg {} {}".format(channel, word_eol[2]))
+        parts = channel.split(":")
+        if len(parts) == 2:
+            ctx = hexchat.find_context(server=parts[0], channel=parts[1])
+            ctx.command("msg {} {}".format(parts[1], word_eol[2]))
+        else:
+            hexchat.command("msg {} {}".format(channel, word_eol[2]))
 
     return hexchat.EAT_ALL
 
@@ -23,6 +28,6 @@ def on_unload(userdata):
     del hooks[:]
     print("Send Multiple Channels unloaded")
 
-hooks.append(hexchat.hook_command("msgchannels", msgchannels, "Command is /msgchannels <channel_list> <msg>"))
+hooks.append(hexchat.hook_command("msgchannels", msgchannels, "Command is /msgchannels <[server:]channel_list> <msg>. Example: /msgchannels freenode:#python,Rizon#news Hello World!"))
 hexchat.hook_unload(on_unload)
 print("Send Multiple Channels v{} loaded".format(__module_version__))
